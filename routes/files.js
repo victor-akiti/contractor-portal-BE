f7ccd1525b00e7158cfb95d7a2001fd1a223b459
Victor Izu-Akiti <victor.izu-akiti@amni.com>
2025-10-17 12:43:23 +0100

    refactor: increase file upload size to 20mb

--- routes/files.js
+++ routes/files.js
@@ -1,7 +1,10 @@
 const { uploadFiles } = require("../controllers/files/upload")
 const authenticate = require("../middleWare/authenticateRequests")
 const multer = require("multer")
-const upload = multer({dest:  __dirname+  "uploads"})
+const upload = multer({
+    dest:  __dirname+  "uploads", 
+    limits: { fileSize: 25 * 1024 * 1024 },
+})
 
 const Router = require("express").Router()
 
